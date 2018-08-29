3b8031e52de41d93039ab3c01fd1722025c68110
Joey Saha <joeysaha@hotmail.com>
2018-08-29 16:50:50 -0400

    updated seeds/validations such that you can break validations with the seed but not as the user

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -6,10 +6,24 @@ class Pledge < ApplicationRecord
   validates :dollar_amount, numericality: { only_float: true }
   validates :user, presence: true
   validate :user_cannot_back_own_project
+  validate :cannot_pledge_if_end_date_is_in_past
+  validate :cannot_pledge_if_start_date_is_in_future  #comment this code out before rails db:seed
 
   def user_cannot_back_own_project
     if project.user == user
       errors.add(:user_id, "cannot back own project")
+    end
+  end
+
+  def cannot_pledge_if_start_date_is_in_future
+    if project.start_date > DateTime.now
+      errors.add(:user_id, "cannot pledge a project before the start date.")
+    end
+  end
+
+  def cannot_pledge_if_end_date_is_in_past
+    if project.end_date < DateTime.now
+      errors.add(:user_id, "cannot pledge a project after the end date.")
     end
   end
 end
